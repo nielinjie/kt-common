1cda20de7097eb9b7c486f175f6e71677186b43b
nielinjie <nielinjie@gmail.com>
2024-03-16 17:35:30 +0800

    depend checking

---
+++ src/commonMain/kotlin/xyz/nietongxue/common/base/diff.kt
@@ -0,0 +1,27 @@
+package xyz.nietongxue.common.base
+
+interface Diff {
+    class Removed(val ids: List<Id>) : Diff
+    class Added(val ids: List<Id>) : Diff
+    class Changed(val ids: List<Id>) : Diff
+
+}
+
+class Diffs(val removed: Diff.Removed, val added: Diff.Added, val changed: Diff.Changed)
+
+fun <T> diff(fromOld: List<T>, toNew: List<T>, idGetter: IdGetter<T>, isChanged: (T, T) -> Boolean): Diffs {
+    val fromIds = fromOld.mapNotNull { idGetter(it) }
+    val toIds = toNew.mapNotNull { idGetter(it) }
+    val removed = fromIds - toIds.toSet()
+    val added = toIds - fromIds.toSet()
+    val changed = fromIds.intersect(toIds.toSet()).filter { id ->
+        val fromE = fromOld.find { idGetter(it) == id }!!
+        val toE = toNew.find { idGetter(it) == id }!!
+        isChanged(fromE, toE)
+    }
+    return Diffs(
+        Diff.Removed(removed),
+        Diff.Added(added),
+        Diff.Changed(changed)
+    )
+}
